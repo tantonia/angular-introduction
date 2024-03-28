6e7bddc4f16ce0bab66cb737cf829b3a3e6fb202
A. Tsatsouli <tantonia01@gmail.com>
2024-03-28 20:01:53 +0200

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'; // controller or model
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
