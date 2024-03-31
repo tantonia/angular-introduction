b9770ffc4f2f586e7947e8c981bb0c2a39cfb4ed
A. Tsatsouli <tantonia01@gmail.com>
2024-03-31 12:36:14 +0300

    new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core'; // controller or model
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name = 'Antonia';
 
-  person = {
-    givenName: 'Antonia',
-    surName: 'Tsatsouli',
-    age: 0x29,
-    email: "tantonia@aueb.gr",
-  };
 }
