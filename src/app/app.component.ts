3c004d920024ac27bce7eabad9c5c200259af97e
A. Tsatsouli <tantonia01@gmail.com>
2024-03-31 13:27:41 +0300

    component input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,19 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 export class AppComponent {
   name = 'Antonia';
 
+  person0 = {
+    givenName: 'Antonia',
+    surName: 'Tsatsouli',
+    age: 0x29,
+    email: "tantonia@aueb.gr",
+    address: "Athens, Greece",
+  };
+
+  person1 = {
+    givenName: 'John',
+      surName: 'Doe',
+      age: 0x25,
+      email: "john@example.gr",
+      address: "New York, USA",
+    };
 }
