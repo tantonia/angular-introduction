cec89e171efad143fbf37d2329d4ed7770e1be00
A. Tsatsouli <tantonia01@gmail.com>
2024-04-03 11:44:31 +0300

    angular material

--- src/app/components/component-output-example/component-output-example.component.ts
+++ src/app/components/component-output-example/component-output-example.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { EPerson, ManyPerson } from 'src/app/shared/interfaces/person';
 import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.component';
+import {Dialog, DialogRef, DIALOG_DATA, DialogModule} from '@angular/cdk/dialog';
+import { PersonTableComponent } from '../person-table/person-table.component';
 
 @Component({
   selector: 'app-component-output-example',
   standalone: true,
-  imports: [ SimpleDatatableComponent],
+  imports: [ SimpleDatatableComponent, DialogModule],
   templateUrl: './component-output-example.component.html',
   styleUrl: './component-output-example.component.css'
 })
 export class ComponentOutputExampleComponent {
   manyPerson = ManyPerson;
 
+  constructor(public dialog: Dialog) {}
+
   onPersonClicked(person: EPerson) {
-    alert(this.personTemplate(person));
+    // alert(this.personTemplate(person));
+    this.dialog.open(PersonDialogComponent, {
+      data: person,
+    });
   }
 
   personTemplate(person: EPerson) {
@@ -25,6 +32,34 @@ export class ComponentOutputExampleComponent {
     Age: ${person.age}
     Email: ${person.email}
     Education: ${person.education}
-    `
+    `;
   }
+}
+
+@Component({
+  imports: [PersonTableComponent],
+  standalone: true,
+  template: `
+  <app-person-table [person]="person"></app-person-table>
+  <button class="btn btn-primary btn-sm" (click)="dialogRef.close()">
+  Close
+  </button>
+  `,
+  styles: [
+    `
+      :host {
+        display: block;
+        background: #fff;
+        border-radius: 8px;
+        padding: 16px;
+        max-width: 500px;
+      }
+    `,
+  ],
+})
+class PersonDialogComponent {
+  constructor(
+    public dialogRef: DialogRef,
+    @Inject(DIALOG_DATA) public person: EPerson,
+  ) {}
 }
