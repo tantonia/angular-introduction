be486223e3d50483d4ad2b10913b5e26f1a1c3a8
A. Tsatsouli <tantonia01@gmail.com>
2024-04-09 11:15:03 +0300

    template driven forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,6 @@ import { ForDirectiveExampleComponent } from './components/for-directive-example
 import { SimpleDatatableExampleComponent } from './components/simple-datatable-example/simple-datatable-example.component';
 import { ComponentOutputExampleComponent } from './components/component-output-example/component-output-example.component';
 import { TemplateDrivenFormExampleComponent } from './components/template-driven-form-example/template-driven-form-example.component';
-import { ReactiveFormExampleComponent } from './components/reactive-form-example/reactive-form-example.component';
-
 
 
 export const routes: Routes = [
@@ -18,5 +16,4 @@ export const routes: Routes = [
     { path: '', component: WelcomeComponent},
     { path: 'component-output-example', component: ComponentOutputExampleComponent},
     { path: 'template-driven-form-example', component: TemplateDrivenFormExampleComponent},
-    { path: 'reactive-form-example', component: ReactiveFormExampleComponent}
 ];
