6ba38fb4f86d4dd309db92da9ebbccd64a01689b
A. Tsatsouli <tantonia01@gmail.com>
2024-04-23 13:24:51 +0300

    login user finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core'; // controller or model
-import { PersonTableComponent } from './components/person-table/person-table.component';
-import { Person } from './shared/interfaces/person';
-import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ListGroupMenuComponent } from './components/list-group-menu/list-group-menu.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +9,8 @@ import { ListGroupMenuComponent } from './components/list-group-menu/list-group-
   imports: [ 
     RouterLink,
     RouterOutlet,
-    ListGroupMenuComponent
+    ListGroupMenuComponent,
+    NavbarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, effect, inject, signal } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Credentials, LoggedInUser, User } from '../interfaces/user';
+import { Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
 
 const API_URL = `${environment.apiURL}/user`
 @Injectable({
@@ -9,10 +11,21 @@ const API_URL = `${environment.apiURL}/user`
 })
 export class UserService {
   http: HttpClient = inject(HttpClient);
+  router: Router = inject(Router);
 
   user = signal<LoggedInUser | null>(null);
 
   constructor() {
+    const access_token = localStorage.getItem('access_token');
+      if(access_token) {
+        const decodedTokenSubject = jwtDecode(access_token)
+        .sub as unknown as LoggedInUser;
+      this.user.set({
+        fullname: decodedTokenSubject.fullname,
+        email: decodedTokenSubject.email,
+      });
+    }
+
     effect(() => {
       if (this.user()) {
         console.log('User logged-in', this.user().fullname);
@@ -32,5 +45,11 @@ export class UserService {
 
   loginUser(credentials: Credentials) {
     return this.http.post<{access_token: string}>(`${API_URL}/login`, credentials)
+  }
+
+  logoutUser() {
+    this.user.set(null);
+    localStorage.removeItem('access_token');
+    this.router.navigate(['login']);
   }
 }
