dfce55e2267c3b9e6f291421b53a5193d79b5a5e
A. Tsatsouli <tantonia01@gmail.com>
2024-04-23 19:18:25 +0300

    fun for nerds

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,8 @@ import { UserRegistrationComponent } from './components/user-registration/user-r
 import { RestrictedContentExampleComponent } from './components/restricted-content-example/restricted-content-example.component';
 import { authGuard } from './shared/guards/auth.guard';
 import { UserLoginComponent } from './components/user-login/user-login.component';
+import { FunForNerdsComponent } from './components/fun-for-nerds/fun-for-nerds/fun-for-nerds.component';
+
 
 export const routes: Routes = [
     { path: 'component-input-example', component: ComponentInputExampleComponent },
@@ -25,5 +27,6 @@ export const routes: Routes = [
     { path: 'user-registration-example', component: UserRegistrationComponent },
     { path: 'restricted-content-example', component: RestrictedContentExampleComponent, canActivate: [authGuard] },
     { path: 'login', component: UserLoginComponent },
+    { path: 'fun-for-nerds', component: FunForNerdsComponent }, 
     { path: '', component: WelcomeComponent },    
 ];
